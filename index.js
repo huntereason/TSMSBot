14e87f55735d51de3bd2f0f319c1c7ad01b11c50
Hunter Eason <huntereasonearl@yahoo.com>
2018-05-29 23:13:04 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+//================================================================
+
 const config = require("./config.json");
 const Discord = require("discord.js");
 const Enmap = require("enmap");
